b2da9c55b5be01be5e342caf48285fc1999aee4e
David Elì <davidelimg1@gmail.com>
2020-08-24 18:52:35 -0500

    Added NavLink comp

--- src/components/HomePage.js
+++ src/components/HomePage.js
@@ -1,11 +1,14 @@
-import React from "react";
+import React from 'react';
+import { Link } from 'react-router-dom';
 
 const HomePage = () => {
   return (
     <div className="jumbotron">
       <h1>Pluralsight Administration</h1>
       <p>React, FLuc, and React Router fro ulta-responsive web apps.</p>
-      <a href="/about">About</a>
+      <Link to="about" className="btn btn-primary">
+        About
+      </Link>
     </div>
   );
 };
