1ddfb2c8cc1d1f4ddd6a6e71125a217f4cf55a0e
David Elì <davidelimg1@gmail.com>
2020-08-24 21:25:04 -0500

    Create Course Form

--- src/components/App.js
+++ src/components/App.js
@@ -3,7 +3,7 @@ import HomePage from './HomePage';
 import AboutPage from './AboutPage';
 import Header from './common/Header';
 import CoursesPage from './CoursesPage';
-import { Route, Switch, Redirect, Router } from 'react-router-dom';
+import { Route, Switch, Redirect } from 'react-router-dom';
 import NotFoundPage from './NotFoundPage';
 import ManageCoursePage from './ManageCoursePage';
 
@@ -16,6 +16,7 @@ const App = () => {
         <Route path="/courses" component={CoursesPage} />
         <Route path="/about" component={AboutPage} />
         <Route path="/course/:slug" component={ManageCoursePage} />
+        <Route path="/course" component={ManageCoursePage} />
         <Redirect from="/about-page" to="/about" />
         <Route component={NotFoundPage} />
       </Switch>
